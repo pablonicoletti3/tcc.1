392a24fd789f104058c114e89029e454374fcff5
pablonicoletti3 <113150304+pablonicoletti3@users.noreply.github.com>
2023-04-13 22:13:40 -0300

    feito 2 serviços

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <nav>
+    <h1>Serviços</h1>
+
+<br>
+
+
+   <img src="imgs/american.jpg" >
+    <h3>Corte americano <br>
+R$ 25,00
+    </h2> 
+
+    <button  >Confirmar</button></button>
+
+<br>
+<br>
+<img src="imgs/mid.jpg" >
+    <h3>Corte Mid Fade Redondo <br>
+        R$ 30,00
+    </h2> 
+
+    <button  >Confirmar</button></button>
+
+
+    <img class="" src="imgs/ ">
+    <h3>Corte Mid Fade Redondo <br>
+        R$ 30,00
+    </h2> 
+
+    <button  >Confirmar</button></button>
+    </nav>
+</body>
+</html>
