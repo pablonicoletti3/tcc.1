eddcd0609aa3198ab12522e50ca785999b7aa318
pablonicoletti3 <113150304+pablonicoletti3@users.noreply.github.com>
2023-04-27 20:38:45 -0300

    salvo1/7

--- index.php
+++ index.php
@@ -1,54 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="index.css">
-</head>
-<body>
-    <nav>
-    <h1>Serviços</h1>
 
-<br>
+<?php
+include "cabecalho.php";
+include "menu.php";
+?>
 
+<div id="carouselExampleFade" class="carousel slide carousel-fade">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imgs/american.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imgs/hihg.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imgs/mid.jpg" class="d-block w-100" alt="..." width="200px">
+    </div>
+    <div class="carousel-item">
+      <img src="img/redDead-image.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
-   <img src="imgs/american.jpg" >
-    <h3>Corte americano <br>
-R$ 25,00
-    </h3> 
+<div class="container mt-5 mb-3">
+  <div class="row">
+    <div class="col-12 text-center"><h1>Jogos em destaque</h1></div>
+    <div class="col-12  text-center mt-3 mb-3"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione nam, sint itaque officiis vitae laboriosam voluptates necessitatibus! Doloribus, consectetur voluptate. Ad temporibus odit porro quam est, consequuntur ratione voluptatibus nulla.</p></div>
+  </div>
 
-    <button  >Confirmar</button></button>
+  <div class="row">
 
-<br>
-<br>
-<img src="imgs/mid.jpg" >
-    <h3>Corte Mid Fade Redondo <br>
-        R$ 30,00
-    </h3> 
+  <?php
+  $servidor_bd = "127.0.0.1";
+  $usuario_bd = "root";
+  $senha_bd = "";
+  $banco_de_dados = "nicoletti1";
 
-    <button  >Confirmar</button></button> <br>
-    <br>
+  $conexao = mysqli_connect($servidor_bd, $usuario_bd, $senha_bd, $banco_de_dados);
 
+  $sql_buscar = "select * from jogo";
 
+  $todos_os_jogos = mysqli_query($conexao, $sql_buscar);
 
-    <img class="corte" src="imgs/hihg.jpg">
-    <h3>Corte hihg Fade reto <br>
-        R$ 25,00
-    </h3> 
-    <button  >Confirmar</button></button> <br>
+  while($um_jogo = mysqli_fetch_assoc($todos_os_jogos)):
+    ?>
+    <div class="col-md-3 text-center mb-4">
+    <img src="<?php echo $um_jogo["foto"];?>" class="img-fluid">
+    <h5 class="mt-3 mb-3"><?php echo $um_jogo["titulo"];?></h5>
+    <a href="<?php echo $um_jogo["video"];?>" class="btn btn-outline-primary">VER MAIS!</a>
+  </div>
+ 
+ <?php
+  endwhile;
+  ?>
 
+  </div>
+  <div class="row mt-5">
+    <div class="col-12 text-center"><h2>Entre em contato</h2></div>
+    <div clas="col-12 text-center"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ipsum, quibusdam libero explicabo facere dolor ducimus perferendis fuga. Quo molestias aliquid aperiam dolores fugit error soluta illum consectetur facere labore.</p></div>
+  </div>
 
+  <div class="row">
+  <form action="inserir-contato.php" method="post">
+    <div class="col">
+    <div class="mb-2">
+  <input type="text" class="form-control" name="nome" placeholder="Nome">
+</div>
+<div class="mb-2">
+  <input type="tel" class="form-control" name="telefone" placeholder="Nome">
+</div>
+<div class="mb-2">
+  <textarea class="form-control" name="duvida" placeholder="Dúvida" rows="4"></textarea>
+</div>
+<div class="mb-2 text-center">
+  <button type="submit" class="btn btn-success w-25">ENVIAR</button>
+</div>
+    </div>
+  </form>
+  </div>
 
-    <img class="corte" src="imgs/MOICA.jpg">
-    <h3>Corte moicatrem  <br>
-        R$ 30,00
-    </h3> 
-    <button  >Confirmar</button></button> <br>
+</div>
 
-
-
-    </nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">iGame</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!-- <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li> -->
+      </ul>
+      <form action="login.php" method="post" class="d-flex">
+        <input class="form-control me-2" type="email" placeholder="E-mail">
+        <input class="form-control me-2" type="password" placeholder="Password">
+        <button class="btn btn-outline-success me-2" type="submit">Entrar</button>
+        <a href="novo-usuario.php" class="btn btn-outline-primary" type="submit">Cadastrar</a>
+      </form>
+    </div>
+  </div>
+</nav>  
+</body>
+</html>
